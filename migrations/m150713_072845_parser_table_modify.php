ee185ef15e1b47f702c6c6c540159357ecf34e48
Patryk Radziszewski <pradziszewski@netis.pl>
2015-07-13 09:57:35 +0200

    modify parser table

---
+++ migrations/m150713_072845_parser_table_modify.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150713_072845_parser_table_modify extends Migration
+{
+    protected $schemaName = 'sync';
+
+    public function safeUp()
+    {
+
+        $this->execute("ALTER TABLE $this->schemaName.{{%parsers}} ALTER COLUMN parser_options TYPE jsonb USING (parser_options::jsonb)");
+        $this->execute("ALTER TABLE $this->schemaName.{{%parsers}} RENAME COLUMN class TO model_class");
+        $this->execute("ALTER TABLE $this->schemaName.{{%parsers}} RENAME TO {{%parser_configuration}}");
+
+    }
+
+    public function safeDown()
+    {
+        $this->execute("ALTER TABLE $this->schemaName.{{%parser_configuration}} RENAME COLUMN model_class TO class");
+        $this->execute("ALTER TABLE $this->schemaName.{{%parser_configuration}} ALTER COLUMN parser_options TYPE text");
+        $this->execute("ALTER TABLE $this->schemaName.{{%parser_configuration}} RENAME TO {{%parsers}}");
+    }
+}
